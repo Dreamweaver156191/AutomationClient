52f01a566672d84cffc04fc979789139a8025d2e
francois156191 <francois.vandermerwe@the-lacuna.com>
2023-03-13 13:27:11 +0200

    Initial commit

---
+++ src/main/java/com/example/automationclient/AutomationClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.automationclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomationClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomationClientApplication.class, args);
+	}
+
+}
