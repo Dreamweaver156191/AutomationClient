d659661bdcac539349b618e45837ba4af6109271
francois156191 <francois.vandermerwe@the-lacuna.com>
2023-03-13 14:29:20 +0200

    Added Logging

--- src/main/java/com/example/automationclient/controller/ExampleController.java
+++ src/main/java/com/example/automationclient/controller/ExampleController.java
@@ -1,17 +1,21 @@
 package com.example.automationclient.controller;
 
-
-
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 @RequestMapping("/example")
 @Api(value = "ExampleController", tags = { "ExampleController" })
 public class ExampleController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ExampleController.class);
 
     @GetMapping("/hello")
     @ApiOperation(value = "Get a hello message", response = String.class)
@@ -20,6 +24,7 @@ public class ExampleController {
             @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
     })
     public String hello() {
+        LOGGER.info("Received request for hello message");
         return "Hello, World!";
     }
 }
