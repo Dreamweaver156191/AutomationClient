867e23d92ccd23deee86e72f1105abfa7df7ad80
francois156191 <francois.vandermerwe@the-lacuna.com>
2023-03-15 23:09:14 +0200

    Full Crud. Stable

--- src/main/java/com/example/automationclient/controller/ApiOperation.java
+++
@@ -1,7 +0,0 @@
-package com.example.automationclient.controller;
-
-public @interface ApiOperation {
-    String value();
-
-    Class<String> response();
-}
--- src/main/java/com/example/automationclient/controller/ExampleController.java
+++ src/main/java/com/example/automationclient/controller/ExampleController.java
@@ -1,14 +1,14 @@
 package com.example.automationclient.controller;
 
+import com.example.automationclient.service.ExampleService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/example")
@@ -16,6 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class ExampleController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ExampleController.class);
+
+    private final ExampleService exampleService;
+
+    @Autowired
+    public ExampleController(ExampleService exampleService) {
+        this.exampleService = exampleService;
+    }
 
     @GetMapping("/hello")
     @ApiOperation(value = "Get a hello message", response = String.class)
@@ -25,6 +32,51 @@ public class ExampleController {
     })
     public String hello() {
         LOGGER.info("Received request for hello message");
-        return "Hello, World!";
+        return exampleService.getHelloMessage();
+    }
+
+    @GetMapping("/goodbye")
+    @ApiOperation(value = "Get a goodbye message", response = String.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved goodbye message"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+    })
+    public String goodbye() {
+        LOGGER.info("Received request for goodbye message");
+        return exampleService.getGoodbyeMessage();
+    }
+
+    @PutMapping("/put")
+    @ApiOperation(value = "Update a message", response = String.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully updated message"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+    })
+    public String update() {
+        LOGGER.info("Received request for update message");
+        return exampleService.updateMessage();
+    }
+
+    @PostMapping("/post")
+    @ApiOperation(value = "Post a message", response = String.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully posted message"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+    })
+    public String post() {
+        LOGGER.info("Received request for post message");
+        return exampleService.postMessage();
+    }
+
+    @DeleteMapping("/delete")
+    @ApiOperation(value = "Delete a message", response = String.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully deleted message"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+    }
+    )
+    public String delete() {
+        LOGGER.info("Received request for delete message");
+        return exampleService.deleteMessage();
     }
 }
